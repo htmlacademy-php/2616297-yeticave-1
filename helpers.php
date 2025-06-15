f86b07fbfab46219afe90e57d1156fd24a47e381
xvlvv <vitkovvvvvvv@mail.ru>
2025-06-15 12:31:09 +0200

    refactor: move mime map to constant

--- helpers.php
+++ helpers.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once 'constants.php';
+
 /**
  * Проверяет переданную дату на соответствие формату 'ГГГГ-ММ-ДД'
  *
@@ -286,48 +288,8 @@ function validate(array $data, array $rules): array
  */
 function mime_to_ext(array $mime_types): array
 {
-    $mime_map = [
-        'image/jpeg' => 'jpeg',
-        'image/jpg' => 'jpg',
-        'image/png' => 'png',
-        'image/gif' => 'gif',
-        'image/svg+xml' => 'svg',
-        'image/webp' => 'webp',
-        'image/bmp' => 'bmp',
-        'image/x-icon' => 'ico',
-        'application/pdf' => 'pdf',
-        'application/msword' => 'doc',
-        'application/vnd.openxmlformats-officedocument.wordprocessingml.document' => 'docx',
-        'application/vnd.ms-excel' => 'xls',
-        'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' => 'xlsx',
-        'application/vnd.ms-powerpoint' => 'ppt',
-        'application/vnd.openxmlformats-officedocument.presentationml.presentation' => 'pptx',
-        'text/plain' => 'txt',
-        'text/csv' => 'csv',
-        'application/json' => 'json',
-        'application/xml' => 'xml',
-        'audio/mpeg' => 'mp3',
-        'audio/ogg' => 'ogg',
-        'audio/wav' => 'wav',
-        'audio/webm' => 'weba',
-        'video/mp4' => 'mp4',
-        'video/ogg' => 'ogv',
-        'video/webm' => 'webm',
-        'video/x-msvideo' => 'avi',
-        'video/quicktime' => 'mov',
-        'application/zip' => 'zip',
-        'application/x-rar-compressed' => 'rar',
-        'application/x-7z-compressed' => '7z',
-        'application/x-tar' => 'tar',
-        'application/gzip' => 'gz',
-        'text/html' => 'html',
-        'text/css' => 'css',
-        'application/javascript' => 'js',
-        'application/x-httpd-php' => 'php',
-    ];
-
     foreach ($mime_types as &$mime) {
-        $mime = $mime_map[$mime] ?? $mime;
+        $mime = MIME_MAP[$mime] ?? $mime;
     }
 
     return $mime_types;
