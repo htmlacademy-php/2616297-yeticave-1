e6ad5048e7e60069bb024629b126907033677cd8
xvlvv <vitkovvvvvvv@mail.ru>
2025-06-15 16:40:58 +0200

    feat: implement login page logic

--- login.php
+++ login.php
@@ -14,28 +14,101 @@ require_once 'models/lot.php';
 require_once 'models/user.php';
 require_once 'validators.php';
 
-$conn = require_once 'init.php';
+[$is_auth, $conn] = require_once 'init.php';
 
 $page_title = 'Войти';
 
 $categories_list = get_all_categories($conn);
 
-$page_content = include_template(
-    'login.php',
+$errors = [];
+
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    $page_content = include_template(
+        'login.php',
+        [
+            'categories_list' => $categories_list,
+            'errors' => $errors,
+            'form_data' => $_POST,
+        ],
+    );
+
+    $html_result = include_template(
+        'layout.php',
+        [
+            'categories_list' => $categories_list,
+            'page_title' => $page_title,
+            'is_auth' => $is_auth,
+            'user_name' => $user_name,
+            'page_content' => $page_content,
+        ],
+    );
+
+    print($html_result);
+    die();
+}
+
+$errors = validate(
+    array_merge_recursive($_POST, $_FILES),
     [
-        'categories_list' => $categories_list,
+        'email' => ['required', 'valid_email', email_exists($conn)],
+        'password' => ['required'],
     ],
 );
 
-$html_result = include_template(
-    'layout.php',
-    [
-        'categories_list' => $categories_list,
-        'page_title' => $page_title,
-        'is_auth' => $is_auth,
-        'user_name' => $user_name,
-        'page_content' => $page_content,
-    ],
-);
+if (!empty($errors)) {
+    $page_content = include_template(
+        'login.php',
+        [
+            'categories_list' => $categories_list,
+            'errors' => $errors,
+            'form_data' => $_POST,
+        ],
+    );
 
-print($html_result);
+    $html_result = include_template(
+        'layout.php',
+        [
+            'categories_list' => $categories_list,
+            'page_title' => $page_title,
+            'is_auth' => $is_auth,
+            'user_name' => $user_name,
+            'page_content' => $page_content,
+        ],
+    );
+
+    print($html_result);
+    die();
+}
+
+$result = authenticate_user($conn, $_POST['email'], $_POST['password']);
+$errors = $result['errors'] ?? [];
+
+if (!empty($errors)) {
+    $page_content = include_template(
+        'login.php',
+        [
+            'categories_list' => $categories_list,
+            'errors' => $errors,
+            'form_data' => $_POST,
+        ],
+    );
+
+    $html_result = include_template(
+        'layout.php',
+        [
+            'categories_list' => $categories_list,
+            'page_title' => $page_title,
+            'is_auth' => $is_auth,
+            'user_name' => $user_name,
+            'page_content' => $page_content,
+        ],
+    );
+
+    print($html_result);
+    die();
+}
+
+$_SESSION = array_merge($_SESSION, $result['user_data']);
+
+header('Location: /');
+die();
