d3b15e66fca441f9b1fe7928a443c9742cb120f4
xvlvv <vitkovvvvvvv@mail.ru>
2025-06-15 16:52:47 +0200

    feat: remove mock auth data

--- sign-up.php
+++ sign-up.php
@@ -1,14 +1,8 @@
 <?php
 
-/**
- * @var bool $is_auth Флаг авторизации
- * @var string $user_name Имя пользователя
- */
-
 declare(strict_types=1);
 
 require_once 'helpers.php';
-require_once 'data.php';
 require_once 'models/category.php';
 require_once 'models/lot.php';
 require_once 'models/user.php';
