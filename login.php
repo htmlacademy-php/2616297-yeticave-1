fa8e7c80125839963c65e936b6c39bc6aa5ebd55
xvlvv <vitkovvvvvvv@mail.ru>
2025-06-15 12:12:51 +0200

    feat: add logic for login & sign-up pages and their templates

--- login.php
+++ login.php
@@ -1,1 +1,41 @@
 <?php
+
+/**
+ * @var bool $is_auth Флаг авторизации
+ * @var string $user_name Имя пользователя
+ */
+
+declare(strict_types=1);
+
+require_once 'helpers.php';
+require_once 'data.php';
+require_once 'models/category.php';
+require_once 'models/lot.php';
+require_once 'models/user.php';
+require_once 'validators.php';
+
+$conn = require_once 'init.php';
+
+$page_title = 'Войти';
+
+$categories_list = get_all_categories($conn);
+
+$page_content = include_template(
+    'login.php',
+    [
+        'categories_list' => $categories_list,
+    ],
+);
+
+$html_result = include_template(
+    'layout.php',
+    [
+        'categories_list' => $categories_list,
+        'page_title' => $page_title,
+        'is_auth' => $is_auth,
+        'user_name' => $user_name,
+        'page_content' => $page_content,
+    ],
+);
+
+print($html_result);
--- sign-up.php
+++ sign-up.php
@@ -1,1 +1,71 @@
 <?php
+
+/**
+ * @var bool $is_auth Флаг авторизации
+ * @var string $user_name Имя пользователя
+ */
+
+declare(strict_types=1);
+
+require_once 'helpers.php';
+require_once 'data.php';
+require_once 'models/category.php';
+require_once 'models/lot.php';
+require_once 'models/user.php';
+require_once 'validators.php';
+
+$conn = require_once 'init.php';
+
+$page_title = 'Регистрация';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $hours_in_day = 24;
+
+    $errors = validate(
+        array_merge_recursive($_POST, $_FILES),
+        [
+            'email' => ['required', 'valid_email', unique_email($conn)],
+            'password' => ['required'],
+            'name' => ['required', character_limit(129)],
+            'message' => ['required', character_limit(256)],
+        ],
+    );
+
+    if (empty($errors)) {
+        add_user(
+            $conn,
+            [
+                $_POST['email'],
+                $_POST['name'],
+                password_hash($_POST['password'], PASSWORD_BCRYPT),
+                $_POST['message'],
+            ]
+        );
+
+        header('Location: /login.php');
+    }
+}
+
+$categories_list = get_all_categories($conn);
+
+$page_content = include_template(
+    'sign-up.php',
+    [
+        'categories_list' => $categories_list,
+        'errors' => $errors ?? [],
+        'form_data' => $_POST,
+    ],
+);
+
+$html_result = include_template(
+    'layout.php',
+    [
+        'categories_list' => $categories_list,
+        'page_title' => $page_title,
+        'is_auth' => $is_auth,
+        'user_name' => $user_name,
+        'page_content' => $page_content,
+    ],
+);
+
+print($html_result);
