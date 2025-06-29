19dce35ba568fdaaf0f0a4b0ca002d6283a11463
xvlvv <vitkovvvvvvv@mail.ru>
2025-06-29 12:42:28 +0200

    feat: add my-bets page with template

---
+++ my-bets.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+require_once 'helpers.php';
+require_once 'models/category.php';
+require_once 'models/lot.php';
+
+$conn = require_once 'init.php';
+
+$is_auth = is_authorized();
+$user_name = get_user_name();
+$page_title = 'Мои ставки';
+
+if ($is_auth === false) {
+    exit_with_message('Доступ запрещён', 403);
+}
+
+$bids = get_user_bids($conn, get_user_id());
+$categories_list = get_all_categories($conn);
+
+$page_content = include_template(
+    'my-bets.php',
+    [
+        'categories_list' => $categories_list,
+        'bids' => $bids,
+    ],
+);
+
+$html_result = include_template(
+    'layout.php',
+    [
+        'categories_list' => $categories_list,
+        'page_title' => $page_title,
+        'is_auth' => $is_auth,
+        'user_name' => $user_name,
+        'page_content' => $page_content,
+    ],
+);
+
+print($html_result);
---
+++ templates/my-bets.php
@@ -0,0 +1,86 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * @var string[] $categories_list Список категорий
+ */
+?>
+<main>
+    <nav class="nav">
+        <ul class="nav__list container">
+            <?php foreach ($categories_list as $category): ?>
+                <li class="nav__item">
+                    <a href="all-lots.html"><?= $category['name'] ?? ''; ?></a>
+                </li>
+            <?php endforeach; ?>
+        </ul>
+    </nav>
+    <section class="rates container">
+        <h2>Мои ставки</h2>
+        <?php if (!empty($bids)): ?>
+        <table class="rates__list">
+            <?php
+            foreach ($bids as $bid):
+                $time_ago = to_time_ago_format($bid['created_at']);
+                $end_date = get_dt_range($bid['end_date']);
+                $time_left = implode(':', $end_date);
+                $user_won = $bid['is_winner'] !== null;
+                $is_finished = $end_date['hours'] === 0 && $end_date['minutes'] === 0 && $end_date['seconds'] === 0;
+                $is_less_than_hour_left = $end_date['hours'] === 0;
+
+                $bid_modifier = match (true) {
+                    $user_won => 'rates__item--win',
+                    $is_finished => 'rates__item--end',
+                    default => '',
+                };
+
+                $timer_content = match (true) {
+                    $user_won => 'Ставка выиграла',
+                    $is_finished => 'Торги окончены',
+                    default => $time_left,
+                };
+
+                $timer_modifier = match (true) {
+                    $user_won => 'timer--win',
+                    $is_finished => 'timer--end',
+                    $is_less_than_hour_left => 'timer--finishing',
+                    default => '',
+                };
+            ?>
+            <tr class="rates__item <?= $bid_modifier; ?>">
+                <td class="rates__info">
+                    <div class="rates__img">
+                        <img
+                            src="<?= $bid['img_url'] ?? ''; ?>"
+                            width="54"
+                            height="40"
+                            alt="<?= htmlspecialchars($bid['name'] ?? ''); ?>"
+                        >
+                    </div>
+                    <h3 class="rates__title">
+                        <a href="/lot.php?id=<?= $bid['id'] ?? 0; ?>">
+                            <?= htmlspecialchars($bid['name'] ?? ''); ?>
+                        </a>
+                    </h3>
+                </td>
+                <td class="rates__category">
+                    <?= $bid['category_name'] ?? ''; ?>
+                </td>
+                <td class="rates__timer">
+                    <div class="timer <?= $timer_modifier; ?>">
+                        <?= $timer_content; ?>
+                    </div>
+                </td>
+                <td class="rates__price">
+                    <?= format_price($bid['current_price'] ?? ''); ?>
+                </td>
+                <td class="rates__time">
+                    <?= $time_ago; ?>
+                </td>
+            </tr>
+            <?php endforeach; ?>
+        </table>
+        <?php endif; ?>
+    </section>
+</main>
